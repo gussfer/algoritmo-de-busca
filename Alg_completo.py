2bc91fc5c55d60008ac33fd81cc8296612baa4a5
gussfer <oliveira.gustavo@unifesp.br>
2023-09-09 16:20:58 -0300

    first commit

---
+++ Alg_completo.py
@@ -0,0 +1,147 @@
+class No:
+    def __init__(self, nome: str, funcao_h: float):
+        self.nome = nome
+        self.funcao_h = funcao_h
+        self.funcao_f = 0
+        self.funcao_g = 0
+        self.adjacentes = []
+        self.no_antecessor = None
+
+    def get_nome(self):
+        return self.nome
+
+    def get_funcao_h(self):
+        return self.funcao_h
+
+    def get_funcao_f(self):
+        return self.funcao_f
+
+    def get_funcao_g(self):
+        return self.funcao_g
+
+    def get_no_antecessor(self):
+        return self.no_antecessor
+
+    def get_adjacentes(self):
+        return self.adjacentes
+
+    def set_funcao_g(self, funcao_g: float):
+        self.funcao_g = funcao_g
+
+    def set_funcao_f(self, funcao_f: float):
+        self.funcao_f = funcao_f
+
+    def set_no_antecessor(self, no_antecessor):
+        self.no_antecessor = no_antecessor
+
+    def set_adjacentes(self, adjacentes: list):
+        self.adjacentes = adjacentes
+
+class Aresta:
+    def __init__(self, custo: float, no_alvo: No):
+        self.custo = custo
+        self.no_alvo = no_alvo
+
+    def get_custo(self):
+        return self.custo
+
+    def get_no_alvo(self):
+        return self.no_alvo
+
+class AEstrela:
+    def busca_a_estrela(self, no_origem: No, no_destino: No):
+        prioridades = []
+        explorados = set()
+
+        if no_origem == no_destino:
+            return []
+
+        prioridades.append(no_origem)
+
+        while prioridades:
+            no_atual = self.menor_funcao_f(prioridades)
+            prioridades.remove(no_atual)
+
+            if no_atual == no_destino:
+                return self.percorrer_caminho(no_atual)
+
+            explorados.add(no_atual)
+
+            for aresta_temp in no_atual.get_adjacentes():
+                no_filho = aresta_temp.get_no_alvo()
+                g_temp = no_atual.get_funcao_g() + aresta_temp.get_custo()
+                f_temp = g_temp + no_filho.get_funcao_h()
+
+                if no_filho in explorados and f_temp >= no_filho.get_funcao_f():
+                    continue
+
+                if no_filho not in prioridades or f_temp < no_filho.get_funcao_f():
+                    no_filho.set_no_antecessor(no_atual)
+                    no_filho.set_funcao_g(g_temp)
+                    no_filho.set_funcao_f(f_temp)
+
+                    if no_filho not in prioridades:
+                        prioridades.append(no_filho)
+
+        return None
+
+    def menor_funcao_f(self, prioridades):
+        return min(prioridades, key=lambda x: x.get_funcao_f())
+
+    def percorrer_caminho(self, alvo: No):
+        caminho = []
+        while alvo:
+            caminho.insert(0, alvo.get_nome())
+            alvo = alvo.get_no_antecessor()
+        return caminho
+
+
+# Main
+arad = No("Arad", 366)
+bucarest = No("Bucarest", 0)
+craiova = No("Craiova", 160)
+dobreta = No("Dobreta", 242)
+eforie = No("Eforie", 161)
+fagaras = No("Fagaras", 176)
+giurgiu = No("Giurgiu", 77)
+hirsova = No("Hirsova", 151)
+iasi = No("Iasi", 226)
+lugoj = No("Lugoj", 244)
+mehadia = No("Mehadia", 241)
+neamt = No("Neamt", 234)
+oradea = No("Oradea", 380)
+pitesti = No("Pitesti", 100)
+rimnicuVilcea = No("Rimnicu Vilcea", 193)
+sibiu = No("Sibiu", 253)
+timisoara = No("Timisoara", 329)
+urziceni = No("Urziceni", 80)
+vaslui = No("Vaslui", 199)
+zerind = No("Zerind", 374)
+
+arad.set_adjacentes([Aresta(75, zerind), Aresta(140, sibiu), Aresta(118, timisoara)])
+bucarest.set_adjacentes([Aresta(85, urziceni), Aresta(211, fagaras), Aresta(101, pitesti), Aresta(90, giurgiu)])
+craiova.set_adjacentes([Aresta(138, pitesti), Aresta(146, rimnicuVilcea), Aresta(120, dobreta)])
+dobreta.set_adjacentes([Aresta(120, craiova), Aresta(75, mehadia)])
+eforie.set_adjacentes([Aresta(86, hirsova)])
+fagaras.set_adjacentes([Aresta(99, sibiu), Aresta(211, bucarest)])
+giurgiu.set_adjacentes([Aresta(90, bucarest)])
+hirsova.set_adjacentes([Aresta(86, eforie)])
+iasi.set_adjacentes([Aresta(92, vaslui), Aresta(87, neamt)])
+lugoj.set_adjacentes([Aresta(111, timisoara), Aresta(70, mehadia)])
+mehadia.set_adjacentes([Aresta(75, dobreta), Aresta(70, lugoj)])
+neamt.set_adjacentes([Aresta(87, iasi)])
+oradea.set_adjacentes([Aresta(71, zerind), Aresta(151, sibiu)])
+pitesti.set_adjacentes([Aresta(97, rimnicuVilcea), Aresta(101, bucarest), Aresta(138, craiova)])
+rimnicuVilcea.set_adjacentes([Aresta(97, pitesti), Aresta(146, craiova), Aresta(80, sibiu)])
+sibiu.set_adjacentes([Aresta(99, fagaras), Aresta(80, rimnicuVilcea), Aresta(140, arad), Aresta(151, oradea)])
+timisoara.set_adjacentes([Aresta(118, arad), Aresta(111, lugoj)])
+urziceni.set_adjacentes([Aresta(85, bucarest), Aresta(142, vaslui), Aresta(98, hirsova)])
+vaslui.set_adjacentes([Aresta(92, iasi), Aresta(142, urziceni)])
+zerind.set_adjacentes([Aresta(71, oradea), Aresta(75, arad)])
+
+a_estrela = AEstrela()
+caminho = a_estrela.busca_a_estrela(arad, bucarest)
+if caminho:
+    print("Caminho:", caminho)
+else:
+    print("Caminho não encontrado.")
