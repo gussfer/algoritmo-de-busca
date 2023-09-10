d4bced7e1cfe8d11ba83438fcd227add7f6e14bc
gussfer <oliveira.gustavo@unifesp.br>
2023-09-10 12:39:24 -0300

    comentando-correção-classe AEstrela

--- Alg_completo.py
+++ Alg_completo.py
@@ -50,44 +50,54 @@ class Aresta:
 
 class AEstrela:
     def busca_a_estrela(self, no_origem: No, no_destino: No):
+        #criar as listas vazias
         prioridades = []
-        explorados = set()
-
-        if no_origem == no_destino:
-            return []
-
+        explorados = []
+        #listaPrioridade adiciona origem
         prioridades.append(no_origem)
 
-        while prioridades:
+        #se origem == destino finaliza
+        if no_origem == no_destino:
+            return []
+        no_atual = prioridades[0]
+        #Enquanto filaPrioridade não vazia e destino não encontrado
+        while len(prioridades)>0 and (no_atual != no_destino):
+            # No atual = no com menor função f
             no_atual = self.menor_funcao_f(prioridades)
             prioridades.remove(no_atual)
-
+            # Fila explorados adiciona nó atual
+            explorados.append(no_atual)
+            # Se no atual == destino -> parar
             if no_atual == no_destino:
                 return self.percorrer_caminho(no_atual)
-
-            explorados.add(no_atual)
-
+            # Senao
+            # Para cada aresta adjacente do no atual
             for aresta_temp in no_atual.get_adjacentes():
+                # No filho = aresta.alvo
                 no_filho = aresta_temp.get_no_alvo()
+                # funcaoGTemp = noAtual.FuncaoG() + aresta.custo;
                 g_temp = no_atual.get_funcao_g() + aresta_temp.get_custo()
+                # funcaoFTemp = funcaoGTemp + noFilho.FuncaoH();
                 f_temp = g_temp + no_filho.get_funcao_h()
-
+                # Se caso o nó filho já tenha sido explorado e seu valor de função f é maior que a função f do pai, então ele é desconsiderado
                 if no_filho in explorados and f_temp >= no_filho.get_funcao_f():
                     continue
-
+                # senão se o nó filho não está na filaPrioridades ou sua função f é maior que a funcaoFTemp
                 if no_filho not in prioridades or f_temp < no_filho.get_funcao_f():
+                    # filho.Adjacente = atual;
                     no_filho.set_no_antecessor(no_atual)
+                    # filho.funcaoG = funcaoGTemp;
                     no_filho.set_funcao_g(g_temp)
+                    # filho.funcaoF= funcaoFTemp;
                     no_filho.set_funcao_f(f_temp)
-
+                    # Se filaPrioridades não contem filho --> FilaPrioridades adiciona filho
                     if no_filho not in prioridades:
                         prioridades.append(no_filho)
-
         return None
-
+    # melhorada chatGPT
     def menor_funcao_f(self, prioridades):
         return min(prioridades, key=lambda x: x.get_funcao_f())
-
+    # melhorada chatGP
     def percorrer_caminho(self, alvo: No):
         caminho = []
         while alvo:
